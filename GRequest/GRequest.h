4a28183d6b393d95a1560aee7ce400a5c90f71e9
ruoyuf <lingoerer@gmail.com>
2014-07-05 16:20:17 +0800

    Initial Commit

---
+++ GRequest/GRequest.h
@@ -0,0 +1,19 @@
+//
+//  GRequest.h
+//  GRequest
+//
+//  Created by Ruoyu Fu on 14-7-5.
+//  Copyright (c) 2014年 Roy Fu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GRequest.
+FOUNDATION_EXPORT double GRequestVersionNumber;
+
+//! Project version string for GRequest.
+FOUNDATION_EXPORT const unsigned char GRequestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GRequest/PublicHeader.h>
+
+
---
+++ GRequestTests/GRequestTests.swift
@@ -0,0 +1,35 @@
+//
+//  GRequestTests.swift
+//  GRequestTests
+//
+//  Created by Ruoyu Fu on 14-7-5.
+//  Copyright (c) 2014年 Roy Fu. All rights reserved.
+//
+
+import XCTest
+
+class GRequestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
